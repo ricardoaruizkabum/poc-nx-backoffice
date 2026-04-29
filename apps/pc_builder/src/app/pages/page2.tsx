8471769400b11e22bbb7c0c5c6f264952296274e
Ricardo Ruiz <ricardo.ruiz@kabum.com.br>
2026-04-29 19:52:55 -0300

    feat(ui): initialize UI library with components and configurations

--- apps/pc_builder/src/app/pages/page2.tsx
+++ apps/pc_builder/src/app/pages/page2.tsx
@@ -1,8 +1,14 @@
+import { Button, Input } from "@kbm/ui";
+
 export function Page2() {
   return (
     <div>
       <h2>PC Builder - Page 2</h2>
       <p>Conteúdo da Page 2.</p>
+      <div className="flex">
+        <Button variant='destructive'>Click Here</Button>
+        <Input placeholder="Type your content here"/>
+      </div>
     </div>
   );
 }
--- apps/product/src/app/pages/page1.tsx
+++ apps/product/src/app/pages/page1.tsx
@@ -1,8 +1,15 @@
+import { Button, Input } from "@kbm/ui";
+
 export function Page1() {
   return (
     <div>
       <h2>Product - Page 1</h2>
       <p>Conteúdo da Page 1.</p>
+
+      <div className="flex">
+        <Button variant='default'>Click Here</Button>
+        <Input placeholder="Type your content here"/>
+      </div>
     </div>
   );
 }
