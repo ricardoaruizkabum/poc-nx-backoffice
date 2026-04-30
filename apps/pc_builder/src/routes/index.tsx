6c9622dd3e3c3e3e72aa892b7bcc1f62dc76b751
Ricardo Ruiz <ricardo.ruiz@kabum.com.br>
2026-04-30 14:30:55 -0300

    feat: padronizando as rotas dos apps

---
+++ apps/pc_builder/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import NxWelcome from '../app/nx-welcome';
+import Page1 from '../app/pages/page1';
+import Page2 from '../app/pages/page2';
+
+type Route = {
+  path: string;
+  element: React.ReactNode;
+  index?: boolean;
+};
+
+export const routes: Route[] = [
+  {
+    path: '/',
+    element: <NxWelcome title="Manager CX" />,
+    index: true,
+  },
+  {
+    path: 'page1',
+    element: <Page1 />,
+  },
+  {
+    path: 'page2',
+    element: <Page2 />,
+  },
+];
