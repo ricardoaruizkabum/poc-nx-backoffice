bb5aa5cb67cc0c49811f2413b55cff300219d136
Ricardo Ruiz <ricardo.ruiz@kabum.com.br>
2026-05-12 08:01:20 -0300

    feat: ajuste

--- apps/product/src/app/pages/page1.tsx
+++ apps/product/src/app/pages/page1.tsx
@@ -3,7 +3,7 @@ import { Button, Input } from '@kbm/ui';
 export function Page1() {
   return (
     <div>
-      <h2 className="text-xl font-bold">Produto - Page 1</h2>
+      <h2 className="text-xl font-bold">Produto - Página 1</h2>
       <p className="text-lg font-semibold text-gray-500">Conteúdo da Page 1.</p>
 
       <div className="flex">
