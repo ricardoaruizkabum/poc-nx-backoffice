415bb11a383dd2a6982fe244cca7a9eb4db553a5
Ricardo Ruiz <ricardo.ruiz@kabum.com.br>
2026-05-12 15:10:08 -0300

    feat: alteracao no shell para teste de deploy

--- apps/shell/src/menu/product.tsx
+++ apps/shell/src/menu/product.tsx
@@ -9,7 +9,7 @@ export const productMenu: MenuItem[] = [
     icon: FolderKanban,
     children: [
       {
-        title: 'Opções 1',
+        title: 'Opções',
         url: '/product/page1',
         icon: Settings,
       },
