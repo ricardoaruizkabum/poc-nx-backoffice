edff992851a190fdf812b8d960ecc0b2cd087b6c
Ricardo Ruiz <ricardo.ruiz@kabum.com.br>
2026-05-13 09:47:52 -0300

    feat: ajuste no ui para teste de build

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -43,8 +43,8 @@ const buttonVariants = cva(
 
 function Button({
   className,
-  variant = 'default',
   size = 'default',
+  variant = 'default',
   asChild = false,
   ...props
 }: React.ComponentProps<'button'> &
